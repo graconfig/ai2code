40f2c2e14f51ec65ec996e2c571f8f41674f6636
3749 <yuhang.xiao@hand-china.com>
2025-06-06 15:19:40 +0800

    node 合并前端

--- srv/src/main/java/customer/ai2code/service/execution/BotExecutionFactory.java
+++
@@ -1,6 +0,0 @@
-package customer.ai2code.service.execution;
-
-@FunctionalInterface
-public interface BotExecutionFactory {
-    public BotExecution create(String botName, String description, String version, boolean enabled);
-}
