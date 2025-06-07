f21ddd99cdee7eb589c5de7d621ee025d1bc1c1f
Hongda.Chen <1146900443@qq.com>
2025-06-07 18:59:39 +0800

    界面优化

--- app/taskrun/webapp/controller/TaskRunDetail.controller.js
+++ app/taskrun/webapp/controller/TaskRunDetail.controller.js
@@ -1,24 +1,37 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (
-        Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageToast) {
+    "use strict";
 
-        return Controller.extend("ai.orchestration.taskrun.controller.TaskRunDetail", {
-            onInit: function () {
-                const oRouter = this.getOwnerComponent().getRouter();
-                oRouter.attachRouteMatched(function (oEvent) {
-                    const oArgs = oEvent.getParameter("arguments");
-                    if (oArgs && oArgs.taskRunId) {
-                        this.getView().bindElement({
-                            path: "/Tasks(" + oArgs.taskRunId + ")" 
-                        })
-                    }
-                }.bind(this));
-            }
-        });
-    });
+    return Controller.extend(
+      "ai.orchestration.taskrun.controller.TaskRunDetail",
+      {
+        onInit: function () {
+          const oRouter = this.getOwnerComponent().getRouter();
+          oRouter.attachRouteMatched(
+            function (oEvent) {
+              const oArgs = oEvent.getParameter("arguments");
+              if (oArgs && oArgs.taskRunId) {
+                this.getView().bindElement({
+                  path: "/Tasks(" + oArgs.taskRunId + ")",
+                });
+              }
+            }.bind(this)
+          );
+        },
+
+        onBotInstancePress: function (oEvent) {
+          // 这里可以获取被点击行的数据
+          const oContext = oEvent.getSource().getBindingContext();
+          if (oContext) {
+            const oData = oContext.getObject(); // 你可以在这里处理点击事件，比如弹窗、跳转等
+            MessageToast.show("点击了BotInstance");
+          }
+        },
+      }
+    );
+  }
+);
