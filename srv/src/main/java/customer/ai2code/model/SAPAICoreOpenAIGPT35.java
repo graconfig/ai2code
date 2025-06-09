edb1c4f1a82bef10827a938e8a6243bef3283f87
xibeike <xibeike2024@163.com>
2025-06-09 15:55:55 +0800

    更新Model

---
+++ srv/src/main/java/customer/ai2code/model/SAPAICoreOpenAIGPT35.java
@@ -0,0 +1,36 @@
+package main.java.customer.ai2code.model;
+
+import cds.gen.configservice.ModelConfigs;
+import customer.ai2code.model.AIModel;
+import customer.ai2code.model.config.AIServiceConfig;
+import customer.ai2code.model.config.SAPAICoreConfig;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import static com.sap.ai.sdk.core.JacksonConfiguration.getDefaultObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SAPAICoreOpenAIGPT35 implements AIModel {
+    private ModelConfigs modelConfigs; 
+
+    @Override
+    public String getModelName() {
+        return modelConfigs.getModelName(); 
+    }
+
+    @Override
+    public AIServiceConfig parseModelConfigs() {
+        ObjectMapper mapper = getDefaultObjectMapper();
+        SAPAICoreConfig sapAICoreConfig = mapper.convertValue(modelConfigs.getParameters(), SAPAICoreConfig.class);
+        return sapAICoreConfig;
+    }
+    // @Override
+    // public ModelConfigs getModelConfigs() {
+    //     return modelConfigs;
+    // }
+}
