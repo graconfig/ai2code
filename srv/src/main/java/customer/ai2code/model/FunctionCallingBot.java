24ae755cd88719e966806409d008007060816240
haihua zhang <haihuazhang941106@gmail.com>
2025-06-17 10:25:37 +0800

    1.修改了FunctionCallingBot，修改了构造定义，修改了execute方法

--- srv/src/main/java/customer/ai2code/model/FunctionCallingBot.java
+++ srv/src/main/java/customer/ai2code/model/FunctionCallingBot.java
@@ -1,10 +1,18 @@
 package customer.ai2code.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import cds.gen.configservice.BotTypes;
+import cds.gen.configservice.PromptTexts;
 import cds.gen.mainservice.BotInstances;
 import cds.gen.mainservice.BotInstancesExecuteContext;
+import customer.ai2code.model.config.AIModelResolver;
+import customer.ai2code.service.AIService;
+import customer.ai2code.service.PromptService;
+import customer.ai2code.service.impl.GenericCqnService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,9 +26,32 @@ public class FunctionCallingBot implements Bot {
     private AIModel aiModel;
     private BotTypes botType;
 
+    // 服务依赖（通过构造函数注入）
+    private GenericCqnService genericCqnService;
+    private PromptService promptService;
+    private AIModelResolver aiModelResolver;
+
     @Override
     public BotInstancesExecuteContext.ReturnType execute() {
         // 实现函数调用机器人的执行逻辑
+        List<PromptTexts> prompts = new ArrayList<>();
+        try {
+            // 1. 根据AIModel类型，获取到不同AIService服务
+            AIService aiService = aiModelResolver.resolveAIService(aiModel.getModelConfigs());
+            // 2. 使用genericCqnService.getMainTaskId，再获取Prompt
+            String mainTaskId = genericCqnService.getMainTaskId(botInstance.getId());
+            prompts = promptService.getPrompts(botType.getId(), mainTaskId, botInstance.getId());
+
+            // 3. 根据botType.getImplementationClass()获取到函数调用的实现类
+
+            // 4. 传入类
+
+            // String response = aiService.functionCalling(new ArrayList<>(), prompts, botType.getImplementationClass(), aiModel);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
         return null; // 返回执行结果
     }
 
