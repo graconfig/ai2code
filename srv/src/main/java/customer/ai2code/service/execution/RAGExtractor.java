2dd2f6ae157b9a374d31f738d6200111ad66dabc
haihua zhang <haihuazhang941106@gmail.com>
2025-06-25 15:37:02 +0800

    1.修改了RAGExtractor定义，extract方法返回参数改成List<PromptTexts>

--- srv/src/main/java/customer/ai2code/service/execution/RAGExtractor.java
+++ srv/src/main/java/customer/ai2code/service/execution/RAGExtractor.java
@@ -1,7 +1,9 @@
 package customer.ai2code.service.execution;
 
+import java.util.List;
 import java.util.Locale;
+import cds.gen.configservice.PromptTexts;
 
 public interface RAGExtractor {
-    public String extract(String ragSource, int ragTopK, String query, Locale language, int threshold);
+    public List<PromptTexts> extract(String ragSource, int ragTopK, String query, Locale language, int threshold);
 }
