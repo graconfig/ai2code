9e092c413d73916be0d044e0c1284c4b5f822984
3749 <yuhang.xiao@hand-china.com>
2025-07-01 16:10:34 +0800

    添加JDOC以生成description

--- srv/actions/Functions/callOdata.ts
+++ srv/actions/Functions/callOdata.ts
@@ -1,8 +1,27 @@
+/**
+ * 调用 OData 服务
+ */
 interface odataRequest {
+  /**
+   * 项目名称
+   */
   ProjName: string;
+  /**
+   * 项目描述
+   */
   ProjDesc: string;
   _views: Array<{
+    /**
+     * 视图名称
+     */
     viewName: string;
+    /**
+     * 视图描述
+     */
+    viewDesc: string;
+    /**
+     * 视图源代码
+     */
     sourceCode: string;
   }>;
 }
