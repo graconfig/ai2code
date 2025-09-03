3f8187eafe1569c20fafff34bb98f75ad852ffac
xibeike <xibeike2024@163.com>
2025-09-03 15:53:42 +0800

    1.clas_seo增加字段Custddls

--- srv/src/main/java/customer/ai2code/service/impl/execution/CreateGenclasseoOdataBotExecution.java
+++ srv/src/main/java/customer/ai2code/service/impl/execution/CreateGenclasseoOdataBotExecution.java
@@ -55,7 +55,7 @@ public class CreateGenclasseoOdataBotExecution implements BotExecution {
         newContext.setTrkorr(autoActiveCLASContext.getTrkorr());
         newContext.setReference(autoActiveCLASContext.getReference());
         newContext.setSourcecode(autoActiveCLASContext.getSourcecode());
-        newContext.setCustddls(autoActiveCLASContext.getCustddls);
+        newContext.setCustddls(autoActiveCLASContext.getCustddls());
 
         // 2. 调用OData服务
         String result = "";
