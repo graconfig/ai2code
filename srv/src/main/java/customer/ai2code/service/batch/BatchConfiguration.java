7b9b13e25c060a87d01e0bea64d68b8134c8e79e
haihua zhang <haihuazhang941106@gmail.com>
2025-09-24 23:30:30 +0800

    1

--- srv/src/main/java/customer/ai2code/service/batch/BatchConfiguration.java
+++ srv/src/main/java/customer/ai2code/service/batch/BatchConfiguration.java
@@ -1,6 +1,6 @@
 package customer.ai2code.service.batch;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
@@ -8,14 +8,42 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 /**
  * Spring Batch配置类
- * 配置Job Repository、Job Launcher等核心组件
+ * 扩展DefaultBatchConfiguration并重写getDataSource()方法
+ * 以适配SAP CAP框架的DataSource bean命名规则
  */
 @Configuration
-@EnableBatchProcessing(dataSourceRef = "ds-db", transactionManagerRef = "tx-db")
-public class BatchConfiguration {
+public class BatchConfiguration extends DefaultBatchConfiguration {
+
+    private final DataSource dataSource;
+    private final PlatformTransactionManager transactionManager;
+
+    public BatchConfiguration(DataSource dataSource, PlatformTransactionManager transactionManager) {
+        this.dataSource = dataSource;
+        this.transactionManager = transactionManager;
+    }
+
+    /**
+     * 重写getDataSource()方法，提供SAP CAP环境中的DataSource
+     * 这样Spring Batch就不会期望找到名为'dataSource'的bean
+     */
+    @Override
+    protected DataSource getDataSource() {
+        return this.dataSource;
+    }
+
+    /**
+     * 重写getTransactionManager()方法，提供SAP CAP环境中的TransactionManager
+     */
+    @Override
+    protected PlatformTransactionManager getTransactionManager() {
+        return this.transactionManager;
+    }
 
     /**
      * 配置异步Job Launcher
